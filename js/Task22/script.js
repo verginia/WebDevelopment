177a1799ac8195e6b2904deb9f63c5ad588f0b8c
verginia <geenika@gmail.com>
2025-02-24 20:02:15 +0200

    Added the first three tasks from JavaScript Fundamentals.

---
+++ js/Task22/script.js
@@ -0,0 +1,44 @@
+// Task 1: Creează un fișier HTML și un fișier JS extern. 
+// Leagă fișierul JS de HTML și folosește console.log("Salut, lume!") pentru a verifica dacă legătura funcționează.
+console.log("Task1:")
+console.log("Hello World!")
+console.log("Introduction in JavaScript!")
+
+// Task 2: Declară o constantă numită nume și atribuie-i numele tău. 
+// Declară o variabilă let numită vârstă si atribuiei valoarea 30.
+// Schimbă valoarea variabilei vârstă cu vârsta ta și afișează valoarea în consolă.
+
+console.log("Task2:");
+const name = "Stolear";
+let age = 30;
+
+console.log(name);
+console.log(age);
+age = 36;
+console.log(age);
+
+// Task 3:
+// 1. Creează 2 variabile numite a și b cu valoarea 10 și 5. 
+// Calculează suma(+), diferența(-), produsul(*) și câtul(/). Afișează-le în consolă.
+// 2. Creează 2 variabile numite val1 și val2 cu valoarea 13 și 5. Calculează restul împărțirii dintre cele două numere.
+
+console.log("Task3:");
+// Operatii aritmetice:
+let a = 10;
+let b = 5;
+
+console.log("Addition (Suma): ", a + b);
+console.log("Subtraction (Diferenta): ", a - b);
+console.log("Multiplication (Produsul): ", a * b);
+console.log("Division (Catul): ", a / b);
+
+// Restul impartirii
+let val1 = 13;
+let val2 = 5;
+
+console.log("Modulus (Restul impartirii): ", val1 % val2);
+
+//Task 4:
+
+
+console.log("Task3:");
