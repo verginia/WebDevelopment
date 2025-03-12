1cabc01496dcbcf8cde4524dc7ad00920974193b
verginia <geenika@gmail.com>
2025-03-12 20:46:43 +0200

    Task 9: Javascript DOM.

---
+++ js/Task22/script1.js
@@ -0,0 +1,12 @@
+/*
+1. Creează o pagină HTML simplă care conține un div gol cu id="mesaj". 
+Folosește JavaScript pentru a adăuga textul "Bun venit pe pagina noastră!" în acel div.
+
+Sugestie pentru rezolvare:
+
+-> Creează un fișier HTML cu un div.
+-> Folosește document.getElementById pentru a selecta div-ul.
+-> Adaugă text folosind .innerText.
+ */
+
+document.getElementById("message").innerText = "Welcome to our page! "
